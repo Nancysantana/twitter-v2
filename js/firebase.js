194ccedfe75d7016bfb387262e4861caaebc44e6
Nancysantana <yaretsita.27@gmail.com>
2018-03-13 21:29:33 -0600

    inicio de firebase

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBl4tU270WOKVjgJ2Y3v2RPpSM9ue50OpI",
+    authDomain: "twitter-2a9c2.firebaseapp.com",
+    databaseURL: "https://twitter-2a9c2.firebaseio.com",
+    projectId: "twitter-2a9c2",
+    storageBucket: "twitter-2a9c2.appspot.com",
+    messagingSenderId: "1009187422124"
+  };
+  firebase.initializeApp(config);
+</script>
